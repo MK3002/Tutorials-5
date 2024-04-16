7214b6a5d89f6f834ff37c33e686470a8ff64ab3
MK3002 <162422021+MK3002@users.noreply.github.com>
2024-04-17 01:05:58 +0200

    Delete Dziedziczenie projekt directory

--- Dziedziczenie projekt/src/Technologie.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-public class Technologie extends Employee{
-    public ArrayList<String> technologie;
-    double techBonus=technologie.size()*800;
-
-}
